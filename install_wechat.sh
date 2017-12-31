6576f49d8d853fe0161abb52cee464b62ccfe9a3
ysfl <ysfllxcn@live.com>
2017-12-31 21:44:19 +0800

    修正图标下载地址
    Signed-off-by: ysfl <ysfllxcn@live.com>

--- install_wechat.sh
+++ install_wechat.sh
@@ -4,7 +4,7 @@ app="$HOME/elementaryos"
 file="linux-x64.tar.gz"
 wechat="$path/electronic-wechat-linux-x64"
 url="https://github.com/geeeeeeeeek/electronic-wechat/releases/download/V2.0/linux-x64.tar.gz"
-ico="https://github.com/geeeeeeeeek/electronic-wechat/blob/master/assets/icon.png"
+ico="https://raw.githubusercontent.com/geeeeeeeeek/electronic-wechat/master/assets/icon.png"
 # 安装第三方微信客户端
 
 clear
@@ -12,13 +12,20 @@ clear
 echo '-----------------------------------------------------------------------------'
 echo "创建应用文件夹.app (建议新手把软件安装到此目录)"
 echo '-----------------------------------------------------------------------------'
+if [ ! -f "$wechat/wechat.png" ];then
+  icon="wechat.png"
+else
+  icon="new.wechat.png"
+fi
+  wget -O $icon $ico
+  mv $icon $wechat
 echo "Exec=$wechat/electronic-wechat">>$app/WebChat.desktop
 echo "Icon=$wechat/$icon">>$app/WebChat.desktop
-sudo cp $app/WebChat.desktop /usr/share/applications
-cp $app/WebChat.desktop $HOME/Desktop
+  sudo cp $app/WebChat.desktop /usr/share/applications
+  cp $app/WebChat.desktop $HOME/Desktop
 if [ ! -d $path ];
 then
-mkdir $path
+  mkdir $path
 else
 echo ".app文件夹已存在，将继续安装."
 cd $path
@@ -33,12 +40,6 @@ else
   filename="new.linux-x64.tar.gz"
 fi
   wget -O $filename $url
-if [ ! -f "$wechat/wechat.png" ];then
-    icon="wechat.png"
-else
-    icon="new.wechat.png"
-fi
-    wget -O $icon $ico
 
 echo '-----------------------------------------------------------------------------'
 echo "解压中……"
