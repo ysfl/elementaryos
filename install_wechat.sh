f8b9371012ebe0b7e93c350bcb0042ce44b69a9e
ysfl <ysfllxcn@live.com>
2017-12-31 21:05:36 +0800

    增强微信安装流程
    Signed-off-by: ysfl <ysfllxcn@live.com>

--- install_wechat.sh
+++ install_wechat.sh
@@ -1,27 +1,56 @@
 #!/bin/bash
-
+path="$HOME/.app"
+file="linux-x64.tar.gz"
+wechat="$path/electronic-wechat-linux-x64"
+url="https://github.com/geeeeeeeeek/electronic-wechat/releases/download/V2.0/linux-x64.tar.gz"
+ico="https://github.com/geeeeeeeeek/electronic-wechat/blob/master/assets/icon.png"
 # 安装第三方微信客户端
 
 clear
 
+echo '-----------------------------------------------------------------------------'
 echo "创建应用文件夹.app (建议新手把软件安装到此目录)"
-mkdir $HOME/.app
-cd $HOME/.app
+echo '-----------------------------------------------------------------------------'
+echo "Exec=$path/$wechat/electronic-wechat">>WebChat.desktop
+echo "Icon=$path/$wechat/$icon">>WebChat.desktop
+sudo cp WebChat.desktop /usr/share/applications
+cp WebChat.desktop $HOME/Desktop
+if [ ! -d $path ];
+then
+mkdir $path
+else
+echo ".app文件夹已存在，将继续安装."
+cd $path
+fi
 
-echo ""
+echo '-----------------------------------------------------------------------------'
 echo "正在下载客户端……"
-echo ""
-wget https://github.com/geeeeeeeeek/electronic-wechat/releases/download/V2.0/linux-x64.tar.gz
-
-echo ""
+echo '-----------------------------------------------------------------------------'
+if [! -f $file ];then
+  filename="linux-x64.tar.gz"
+else
+  filename="new.linux-x64.tar.gz"
+fi
+  wget -O $url
+if [! -f $wechat/wechat.png ];then
+  icon="wechat.png"
+else
+  icon="new.wechat.png"
+fi
+  wget -O $ico
+echo '-----------------------------------------------------------------------------'
 echo "解压中……"
-tar -zxvf linux-x64.tar.gz
+echo '-----------------------------------------------------------------------------'
+  tar -zxvf $filename
+  sudo chmod +x /usr/share/applications/WebChat.desktop
+  sudo chmod +x $HOME/Desktop/WebChat.desktop
+  rm $path/$filename
+  sudo rm -R $HOME/elementaryos
 
-echo ""
-echo "安装启动器"
-echo ""
-wget https://github.com/ysfl/elementaryos/blob/master/%E5%BE%AE%E4%BF%A1.desktop
-echo "Exec=$HOME/.app/electronic-wechat-linux-x64/electronic-wechat">>微信.desktop
-echo "Icon=$HOME/.app/electronic-wechat-linux-x64/assets/icon.png">>微信.desktop
-sudo chmod +x /usr/share/applications/微信.desktop
-sudo chmod +x /home/$LOGNAME/Desktop/微信.desktop
+echo '-----------------------------------------------------------------------------'
+echo "微信安装完成！"
+echo '-----------------------------------------------------------------------------'
+echo '制作：ysfl   QQ：1007265870   邮箱：ysfllxcn@live.com   论坛：http://e.ysfl.cn/bbs'
+echo '-----------------------------------------------------------------------------'
+
+  notify-send -i utilities-terminal 微信 "微信安装完成！"
